9b7e1edb9f5e0e237531e354957ca545de6a483f
Tristan Leblanc <tristan@moosefactory.eu>
2017-06-14 14:05:10 +0200

    NSLocalizedString definition

---
+++ Sources/NSBridge.swift
@@ -0,0 +1,13 @@
+//
+//  NSBridge.swift
+//  SendGrid
+//
+//  Created by Tristan Leblanc on 14/06/2017.
+//
+//
+
+import Foundation
+
+public func NSLocalizedString(_ key: String, comment: String) {
+	
+}
